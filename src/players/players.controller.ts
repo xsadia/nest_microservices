8ccc6401cd27e1b01f9902be8f24bad4bfa3e5aa
xsadia <felipe.rosa.arthur27@gmail.com>
2022-04-09 15:22:36 -0300

    feat: inject player service into player controller add endpoints to get users

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreatePlayerDTO } from './dtos/createPlayer.dto';
+import { Player } from './interfaces/player.interface';
+import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
   @Post()
   public async createUpdatePlayer(@Body() createPlayerDTO: CreatePlayerDTO) {
-    const { email } = createPlayerDTO;
-    return JSON.stringify({
-      email,
-    });
+    await this.playersService.createUpdatePlayer(createPlayerDTO);
+    return;
+  }
+
+  @Get()
+  public async getAllPlayers(): Promise<Array<Player>> {
+    return await this.playersService.findAll();
+  }
+
+  @Get(':email')
+  public async getPlayer(@Param('email') email: string): Promise<Player> {
+    return await this.playersService.findByEmail(email);
   }
 }
